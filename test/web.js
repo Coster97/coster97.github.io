8d05134805cc64b82f96ac740410e7b0d5e4aeb8
박은경 <dmsrud1501222@naver.com>
2022-10-10 15:50:01 +0900

    hi

--- test/web.js
+++ test/web.js
@@ -3,10 +3,10 @@ import express from "express";
 const __dirname = "/home/hosting_users/dmsrud1501222/apps/dmsrud1501222_devstudy2022";
 const app = express();
 
-
-app.set("views", __dirname + "/views")
-//app.set("views", process.cwd() + "/views")
 app.set("view engine", "pug")
+app.set('views', __dirname + '/views')
+//app.set("views", process.cwd() + "/views")
+
 app.get("/", (req, res) => {
     res.render("main")
 })
