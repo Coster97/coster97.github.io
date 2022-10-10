4e8e82249f39270cb54e24671aa0ecd6b69e06f7
박은경 <dmsrud1501222@naver.com>
2022-10-10 15:44:15 +0900

    hi

--- test/src/web.js
+++ test/src/web.js
@@ -1,16 +1,16 @@
 import express from "express";
-import path from "path"
 
 const __dirname = "/home/hosting_users/dmsrud1501222/apps/dmsrud1501222_devstudy2022";
 const app = express();
 
 
-
 app.set("views", __dirname + "/views")
 //app.set("views", process.cwd() + "/views")
 app.set("view engine", "pug")
-app.get("/main", (req, res) => {
+app.get("/", (req, res) => {
     res.render("main")
 })
 
 app.listen(8001)
+
+export default app;
