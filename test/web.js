867b4f369c8a308450e4c4ce0f5834a16f967d4e
박은경 <dmsrud1501222@naver.com>
2022-10-10 21:21:21 +0900

    BB

--- test/web.js
+++ test/web.js
@@ -5,8 +5,8 @@ const __dirname = "/home/hosting_users/dmsrud1501222/apps/dmsrud1501222_devstudy
 const app = express();
 
 app.set("view engine", "pug")
-app.set('views', __dirname + '/views')
-//app.set("views", process.cwd() + "/views")
+//app.set('views', __dirname + '/views')
+app.set("views", process.cwd() + "/views")
 
 app.get("/", (req, res) => {
     res.render("main")
