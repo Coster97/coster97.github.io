73971c8969ec449243ebb00f6870083743cf00cd
박은경 <dmsrud1501222@naver.com>
2022-10-10 14:30:58 +0900

    finally

--- test/web.js
+++ test/web.js
@@ -1,8 +1,9 @@
 import express from "express";
 import path from "path"
 
+const __dirname = "/home/hosting_users/dmsrud1501222/apps/dmsrud1501222_devstudy2022";
 const app = express();
-const __dirname = path.resolve();
+
 
 
 app.set("views", __dirname + "/views")
